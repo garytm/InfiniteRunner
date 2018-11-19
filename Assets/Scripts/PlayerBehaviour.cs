a50f6773d005f96a8bf53fb30c25c51c172f1f37
Gary <garytmulhall@gmail.com>
2018-11-19 21:01:32 +0000

    Initial setup

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float movementSpeed = 0.1f;
+    public float jumpHeight = 0.1f;
+    private float jumpDelay = 1.0f;
+
+    private Rigidbody2D playerRigidBody; 
+
+	void Start ()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update ()
+    {
+        jumpDelay += Time.deltaTime;
+        Movement();
+	}
+
+    void Movement()
+    {
+        playerRigidBody.velocity = new Vector2(movementSpeed, playerRigidBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpDelay > 1.0f)
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpHeight);
+            jumpDelay = 0.0f;
+        }
+    }
+}
