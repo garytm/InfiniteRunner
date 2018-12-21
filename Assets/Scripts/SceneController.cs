2e0296cc8e5dff6a5b8415b9a4fddc2d44ebef81
Gary <garytmulhall@gmail.com>
2018-12-21 11:51:54 +0000

    Added a placeholder main menu

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+    public void PlayEndless()
+    {
+        SceneManager.LoadScene("EndlessMode");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    public void HighScores()
+    {
+        SceneManager.LoadScene("HighScoreTable");
+    }
+}
